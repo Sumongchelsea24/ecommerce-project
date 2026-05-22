3751312d1811b0074c6b0931b869e61e27f53b50
Sumongchelsea24 <Sujanchelsea24@gmail.com>
2026-05-22 20:40:18 +0545

    Lift cart up and share it between

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -3,7 +3,7 @@ import "./HomePage.css";
 import axios from "axios";
 import { useEffect, useState } from "react";
 
-export function HomePage() {
+export function HomePage({ cart }) {
   // fetch("http://localhost:3000/api/products")
   //   .then((response) => {
   //     return response.json();
@@ -12,13 +12,10 @@ export function HomePage() {
   //     console.log(data);
   //   });
   const [products, setProducts] = useState([]);
-  const [cart, setCart] = useState([]);
+
   useEffect(() => {
     axios.get("/api/products").then((response) => {
       setProducts(response.data);
-    });
-    axios.get("/api/car-items").then((response) => {
-      setCart(response.data);
     });
   }, []);
 

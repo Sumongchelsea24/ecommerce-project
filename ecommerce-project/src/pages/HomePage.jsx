b24697ee1f6a5d83b362453442453008f332af8c
Sumongchelsea24 <Sujanchelsea24@gmail.com>
2026-05-22 17:46:06 +0545

    Fetch data from the backend

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -3,6 +3,13 @@ import "./HomePage.css";
 import { products } from "../../starting/data/products";
 
 export function HomePage() {
+  fetch("http://localhost:3000/api/products")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+    });
   return (
     <>
       <title>Ecommerce Project</title>
@@ -63,7 +70,6 @@ export function HomePage() {
               </div>
             );
           })}
-         
         </div>
       </div>
     </>

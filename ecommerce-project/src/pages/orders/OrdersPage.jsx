eafd4648e2b92623ce36068b072dd5fd9dad425b
Sumongchelsea24 <Sujanchelsea24@gmail.com>
2026-05-24 15:06:24 +0545

    Seperate into smaller components

--- ecommerce-project/src/pages/orders/OrdersPage.jsx
+++ ecommerce-project/src/pages/orders/OrdersPage.jsx
@@ -1,9 +1,9 @@
 import { useEffect, useState, Fragment } from "react";
-import Header from "../components/Header";
+import Header from "../../components/Header";
 import "./OrdersPage.css";
 import axios from "axios";
 import dayjs from "dayjs";
-import formatMoney from "../utils/money";
+import formatMoney from "../../utils/money";
 
 function OrdersPage({ cart }) {
   const [order, setOrders] = useState([]);

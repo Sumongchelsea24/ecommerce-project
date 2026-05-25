14376dc70cd9efb522ceae0cb0e32dd43db05cc8
Sumongchelsea24 <Sujanchelsea24@gmail.com>
2026-05-25 12:51:18 +0545

    Create add to cart feature

--- ecommerce-project/eslint.config.js
+++ ecommerce-project/eslint.config.js
@@ -1,13 +1,13 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import { defineConfig, globalIgnores } from "eslint/config"; // dependent libraries herera import milaunus
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(["dist"]),
   {
-    files: ['**/*.{js,jsx}'],
+    files: ["**/*.{js,jsx}"],
     extends: [
       js.configs.recommended,
       reactHooks.configs.flat.recommended,
@@ -17,5 +17,13 @@ export default defineConfig([
       globals: globals.browser,
       parserOptions: { ecmaFeatures: { jsx: true } },
     },
+    // RULES BLOCK THAPIEKO:
+    rules: {
+      // useEffect ko dependency check garne warning lai disable gareko
+      "react-hooks/exhaustive-deps": "off",
+
+      // Kahile kahi unused variables le jhyau garcha vane eslai pani direct switch garna sakincha
+      "no-unused-vars": "warn",
+    },
   },
-])
+]);
--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -17,17 +17,27 @@ function App() {
   //   });
   // }, []);
 
+  //yasari use garyo vani reload garnu parxa
+  // useEffect(() => {
+  //   const fetchAppData = async () => {
+  //     const response = await axios.get("/api/cart-items?expand=product");
+  //     setCart(response.data);
+  //   };
+  //   fetchAppData();
+  // }, []);
+
+  const loadCart = async () => {
+    const response = await axios.get("/api/cart-items?expand=product");
+    setCart(response.data);
+  };
+
   useEffect(() => {
-    const fetchAppData = async () => {
-      const response = await axios.get("/api/cart-items?expand=product");
-      setCart(response.data);
-    };
-    fetchAppData();
+    loadCart();
   }, []);
 
   return (
     <Routes>
-      <Route index element={<HomePage cart={cart} />} />
+      <Route index element={<HomePage cart={cart} loadCart={loadCart} />} />
       <Route path="checkout" element={<CheckoutPage cart={cart} />} />
       <Route path="orders" element={<OrdersPage cart={cart} />} />
       <Route path="tracking" element={<TrackingPage />} />

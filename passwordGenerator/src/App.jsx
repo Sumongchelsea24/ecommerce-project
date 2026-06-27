01e474f41c0eb11624cbad59ab7d241e29a53c66
Sumongchelsea24 <Sujanchelsea24@gmail.com>
2026-06-27 11:07:56 +0545

    Currency Conveter Project

--- passwordGenerator/src/App.jsx
+++ passwordGenerator/src/App.jsx
@@ -31,7 +31,7 @@ function App() {
     <>
       <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-8 my-8 text-orange-500 bg-gray-800">
         <h1 className="text-white text-center my-3">Password Generator</h1>
-        <div className="flex shadow rounded-lg overflow-hidden mb-4">
+        <div className="flex shadow rounded-lg overflow-hidden mb-4 bg-white">
           <input
             type="text"
             value={password}

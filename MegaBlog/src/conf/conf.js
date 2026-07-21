1d4e09b9215899bfd9b290504f303f065f803170
Sumongchelsea24 <Sujanchelsea24@gmail.com>
2026-07-21 11:02:55 +0545

    Build Authentication service with appwrite and Appwrite database, file upload and custom queries

---
+++ MegaBlog/src/conf/conf.js
@@ -0,0 +1,9 @@
+const conf = {
+  appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
+  appwriteProjectId: String(import.meta.env.VITE_PROJECT_ID),
+  appwriteDatabaseId: String(import.meta.env.VITE_DATABASE_ID),
+  appwriteCollectionId: String(import.meta.env.VITE_COLLECIION_ID),
+  appwriteBucketId: String(import.meta.env.VITE_PROJECT_ID),
+};
+
+export default conf;
